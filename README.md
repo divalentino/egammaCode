bcbc421782a3d99f46d56424b1c4e850799b1ea8
David Di Valentino <david.di.valentino@cern.ch>
2018-02-19 21:03:05 -0500

    Adding code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # egammaCode
+
+Portfolio giving examples of C++, Python code written for ATLAS egamma reconstruction.
+
+Full, browsable software available here: http://acode-browser.usatlas.bnl.gov/lxr/search?
---
+++ git_commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+message="${1}"
+
+# Do cleanup.
+# rm *~
+# rm -rf pyspark/metastore_db
+# rm -rf pyspark/spark-warehouse
+# rm -rf pyspark/*.pyc
+# rm -rf pyspark/*.log
+
+# #Add new changes.
+# git add *.sh
+# git add *.py
+# git add README.md
+# git add output
+# git add pyspark
+
+git add *
+git commit -m "${message}"
+git push
